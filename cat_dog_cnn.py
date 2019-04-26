d5d7fa653a382cc8989fa917c12dcdd6dfdabaf1
mitalishah25 <32921585+mitalishah25@users.noreply.github.com>
2019-04-25 19:06:09 -0700

    Add files via upload

---
+++ cat_dog_cnn.py
@@ -0,0 +1,59 @@
+# Mitali Shah
+# The code is to build a convolutional neural network using keras libraries to classify images
+# The images contain cat and dog and are divided into training, validation and testing images
+# Data augmentation is performed to increase the number of images while training which helped in achieving good accuracy
+
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
+
+classifier = Sequential()
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Flatten())
+
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Performing data augmentation
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+# Rescaling the test images to match the train images
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 8000,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 2000)
+
+# Save classifier_model to JSON file 
+# This file can be uses directly on testing data without training the model again
+classifier_model_json = classifier.to_json()
+with open("classifier_model.json", "w") as json_file:
+    json_file.write(classifier_model_json)
+# save weights to HDF5
+classifier.save_weights("classifier_weights.h5")
+print("Saved model to disk")
